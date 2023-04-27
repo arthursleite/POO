3f6ab03a81601d2889775d71a5a425699e7191cd
Arthur Leite <81442283+arthursleite@users.noreply.github.com>
2023-04-27 19:18:57 -0300

    Add files via upload

---
+++ Atividade - Herança/packages/application1/AnimalTeste.java
@@ -0,0 +1,20 @@
+package application;
+
+import classes.Animal;
+import classes.Peixe;
+import classes.Cachorro;
+
+public class AnimalTeste {
+
+    public static void main(String[] args) {
+        
+        Animal an = new Animal("Mimosa", 357.5);
+        Peixe pe = new Peixe("Dourado", 5.0, "Agua doce");
+        Cachorro ca = new Cachorro("Bob", 12.3, "Chow Chow");
+        
+        System.out.println("Dados do animal: \n" + an.toString());
+        System.out.println("\nDados do peixe: \n" + pe.toString());
+        System.out.println("\nDados do animal: \n" + ca.toString());
+    }
+    
+}
